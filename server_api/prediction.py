224662f8f6aad0a9f52d8b16fc95d5ed97924fdd
Alexey Maksimov <maksimov.a.vladimi@sberbank.ru>
2021-10-23 11:39:07 +0300

    Fixed error in /social/api/v1.0/getpred added requirements.txt updated docker and checked locally added some code in prediction.py for beauty updated .gitignore: added to ignore some frontend files

--- server_api/prediction.py
+++ server_api/prediction.py
@@ -1,25 +1,26 @@
-import pandas as pd
-from sklearn.metrics.pairwise import cosine_similarity
-import numpy as np
-from sklearn.preprocessing import StandardScaler
-import ast
-import folium
-from shapely.geometry import Point, Polygon
-import shapely.wkt
-import geopandas as gpd
+from typing import Optional, Tuple, Any
+
 import branca.colormap as cm
+import folium
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+import shapely.wkt
+from sklearn.preprocessing import StandardScaler
 
 
-class SocialRecommend():
+class SocialRecommend:
     # Пути к файлам
     PATH_FISHNET_GEO = './data/fishnet_data_geo.csv'
     PATH_FISHNET = './data/fishnet_data_nogeo.csv'
     PATH_MATERS_GEO = './data/maters_data_geo.csv'
     PATH_MATERS = './data/maters_data_nogeo.csv'
-    def __init__(self):
-        self.load_data()
+    MAP_HTML = './templates/map.html'
 
-    def load_data(self):
+    def __init__(self):
+        self._load_data_()
+
+    def _load_data_(self):
         # Читаем базовые файлы
         self.fishnet_base = pd.read_csv(self.PATH_FISHNET)
         self.fishnet_base_geo = pd.read_csv(self.PATH_FISHNET_GEO)
@@ -32,7 +33,7 @@ class SocialRecommend():
             cell = self.fishnet_base_geo[self.fishnet_base_geo['cell_zid'] == r.nearest_cell_zid].index[0]
             self.maters_cells.append(cell)
 
-    def get_normalization_data(self):
+    def get_normalization_data(self) -> Tuple[Any, Any]:
         # Нормализует данные, кроме категориальных.
         # Возвращает 2 нормализованных DataFrame объекта. Информация по всем зонам и по роддомам
         # Нормализация происходит объектом StandartScaler из пакета sklearn, модуля preprocessing
@@ -49,51 +50,50 @@ class SocialRecommend():
         fishnet_std['nearest_hosp'] = self.fishnet_base.nearest_mater.apply(lambda x: self.nearest_hosp(x))
         return fishnet_std, maters_std
 
-
     def migration_calc(self):
         # Перерасчитывает данные self.maters_base и self.fishnet_base с учётом миграции.
         # Нормализовать только после пересчёта миграции.
-        raise('Not Implemented')
+        raise NotImplementedError
 
-    def calculate_dist(self,cell_data,maters):
+    def calculate_dist(self, cell_data, maters) -> Optional[int]:
+        min_dist = None
         for _, row in maters.iterrows():
             min_dist = 10000
             mater_data = np.array(row.values)
-            dist =  np.linalg.norm(cell_data - mater_data)
+            dist = np.linalg.norm(cell_data - mater_data)
             if dist < min_dist:
                 min_dist = dist
         return min_dist
 
-    def predict(self, migration):
+    def predict(self, migration) -> str:
         # Просчитывает метрику по всем ячейкам из fishnet по maters (родильные дома)
         # Возвращаем файл html с построенной картой
+        if migration != '0':
+            raise NotImplementedError
         predictions = []
-        if migration == '0':
-            fishnet, maters = self.get_normalization_data()
-            for index,row in fishnet.iterrows():
-                row_data = np.array(row.values)
-                pred = self.calculate_dist(row_data,maters)
-                predictions.append([pred,index])
-        else:
-            raise('Not Implemented')
-        map = self.build_map(predictions)
-        map.save(outfile='./templates/map.html')
-        string_map = open('./templates/map.html','r').read()
+        fishnet, maters = self.get_normalization_data()
+        for index, row in fishnet.iterrows():
+            row_data = np.array(row.values)
+            pred = self.calculate_dist(row_data, maters)
+            predictions.append([pred, index])
+        built_map = self.build_map(predictions)
+        built_map.save(outfile=self.MAP_HTML)
+        string_map = open(self.MAP_HTML, 'r').read()
         return string_map
 
     @staticmethod
-    def nearest_hosp(x):
+    def nearest_hosp(x) -> int:
         if x <= 3:
             return 1
         return 0
 
     @staticmethod
-    def nearest_mater(x):
+    def nearest_mater(x) -> int:
         if x <= 4:
             return 1
         return 0
 
-    def build_map(self,preds):
+    def build_map(self, preds):
         # строим карту с окрашенными квадратами
         # Зелёный - рекомендовано, жёлтый - 50/50, красный - не рекомендовано
         # avg_mater_dist - среднее минимальное расстояние от роддомов друг к другу
@@ -112,7 +112,7 @@ class SocialRecommend():
                                        style_function=lambda x: {'fillColor': 'blue', 'weight': 0.05,
                                                                  'fillOpacity': 0.2})
                 geo_data = 'В данной области уже построен роддом<br>Номер области: {}<br>'.format(item[1])
-                folium.Popup(geo_data, min_width=250,max_width=250).add_to(geo_j)
+                folium.Popup(geo_data, min_width=250, max_width=250).add_to(geo_j)
                 geo_j.add_to(m_f)
             elif item[0] < 1.5 * avg_mater_dist:
                 sim_geo = gpd.GeoSeries(data_geo).simplify(tolerance=0.001)
@@ -123,7 +123,7 @@ class SocialRecommend():
                 geo_data = 'Рекомендуем данную область для постройки<br>Номер области {}'.format(item[1])
                 geo_data += 'Коэффициент миграционного прироста <input name="cell_param" type="number" value="1"' \
                             'min="0" max="100" style="width: 5em"/>'
-                folium.Popup(geo_data, min_width=250,max_width=280).add_to(geo_j)
+                folium.Popup(geo_data, min_width=250, max_width=280).add_to(geo_j)
                 geo_j.add_to(m_f)
             elif item[0] < 2.5 * avg_mater_dist:
                 sim_geo = gpd.GeoSeries(data_geo).simplify(tolerance=0.001)
@@ -134,7 +134,7 @@ class SocialRecommend():
                 geo_data = 'Данную область можно считать неплохой для постройки<br>Номер области: {}'.format(item[1])
                 geo_data += 'Коэффициент миграционного прироста <input name="cell_param" type="number" value="1"' \
                             'min="0" max="100" style="width: 5em"/>'
-                folium.Popup(geo_data, min_width=250,max_width=320).add_to(geo_j)
+                folium.Popup(geo_data, min_width=250, max_width=320).add_to(geo_j)
                 geo_j.add_to(m_f)
             elif item[0] < 3 * avg_mater_dist:
                 sim_geo = gpd.GeoSeries(data_geo).simplify(tolerance=0.001)
@@ -145,7 +145,7 @@ class SocialRecommend():
                 geo_data = 'Слабо рекомендуем данную область для постройки<br>Номер области: {}'.format(item[1])
                 geo_data += 'Коэффициент миграционного прироста <input name="cell_param" type="number" value="1"' \
                             'min="0" max="100" style="width: 5em"/>'
-                folium.Popup(geo_data, min_width=250,max_width=300).add_to(geo_j)
+                folium.Popup(geo_data, min_width=250, max_width=300).add_to(geo_j)
                 geo_j.add_to(m_f)
             elif item[0] < 4.5 * avg_mater_dist:
                 sim_geo = gpd.GeoSeries(data_geo).simplify(tolerance=0.001)
@@ -156,7 +156,7 @@ class SocialRecommend():
                 geo_data = 'Не рекомендуем данную область для постройки<br>Номер области: {}'.format(item[1])
                 geo_data += 'Коэффициент миграционного прироста <input name="cell_param" type="number" value="1"' \
                             'min="0" max="100" style="width: 5em"/>'
-                folium.Popup(geo_data, min_width=250,max_width=280).add_to(geo_j)
+                folium.Popup(geo_data, min_width=250, max_width=280).add_to(geo_j)
                 geo_j.add_to(m_f)
             else:
                 sim_geo = gpd.GeoSeries(data_geo).simplify(tolerance=0.001)
@@ -169,7 +169,8 @@ class SocialRecommend():
                             'min="0" max="100" style="width: 5em"/>'
                 folium.Popup(geo_data, min_width=250, max_width=320).add_to(geo_j)
                 geo_j.add_to(m_f)
-        colormap = cm.LinearColormap(colors=['red','#FF4000','orange','#C8FE2E','green', 'blue'], index=[0, 0.25, 0.5, 0.75, 1], vmin=0, vmax=1)
+        colormap = cm.LinearColormap(colors=['red', '#FF4000', 'orange', '#C8FE2E', 'green', 'blue'],
+                                     index=[0, 0.25, 0.5, 0.75, 1], vmin=0, vmax=1)
         colormap.caption = 'Целесообразность постройки объекта'
         colormap.add_to(m_f)
         return m_f
